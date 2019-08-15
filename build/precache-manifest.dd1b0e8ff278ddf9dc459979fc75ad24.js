f3cdd7e70f7d965554b280facf267c66076341e9
VitaliiKalinbet <bosssvit@gmail.com>
2019-08-15 12:22:51 +0300

    add gh-pages, and change package.json and readme

---
+++ build/precache-manifest.dd1b0e8ff278ddf9dc459979fc75ad24.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3abc3acef072d8b4954d6fa4bdb9501",
+    "url": "/training_project_for__html_css/index.html"
+  },
+  {
+    "revision": "3b1cb80e380dd0c0225a",
+    "url": "/training_project_for__html_css/static/css/main.5f3772c0.chunk.css"
+  },
+  {
+    "revision": "b483f25db901f764b628",
+    "url": "/training_project_for__html_css/static/js/2.2df2eab5.chunk.js"
+  },
+  {
+    "revision": "3b1cb80e380dd0c0225a",
+    "url": "/training_project_for__html_css/static/js/main.ca7bb703.chunk.js"
+  },
+  {
+    "revision": "1edc488ea26db4971be9",
+    "url": "/training_project_for__html_css/static/js/runtime~main.2562d26d.js"
+  },
+  {
+    "revision": "5892909a4adad8f5bac53b9a22ec8086",
+    "url": "/training_project_for__html_css/static/media/ALARM.5892909a.svg"
+  },
+  {
+    "revision": "b72c272b3fa80c25b727d310daba001c",
+    "url": "/training_project_for__html_css/static/media/BOOK.b72c272b.svg"
+  },
+  {
+    "revision": "2515860bd129e4e5f548435f7d9fdba4",
+    "url": "/training_project_for__html_css/static/media/COMPUTER.2515860b.svg"
+  },
+  {
+    "revision": "b698f5298df75999d9b49e728572de42",
+    "url": "/training_project_for__html_css/static/media/GRAPH.b698f529.svg"
+  },
+  {
+    "revision": "d50d383fd72c6ab65982926c17b13d93",
+    "url": "/training_project_for__html_css/static/media/HOME.d50d383f.svg"
+  },
+  {
+    "revision": "e4b37a19ea226e577a9170632a991bc6",
+    "url": "/training_project_for__html_css/static/media/IMAGE.e4b37a19.svg"
+  },
+  {
+    "revision": "041960c584affe651b6f70e2604b1a77",
+    "url": "/training_project_for__html_css/static/media/Layer1.041960c5.png"
+  },
+  {
+    "revision": "9ed5fbef6bb33c68fb0d86724d690c29",
+    "url": "/training_project_for__html_css/static/media/Layer1@2x.9ed5fbef.png"
+  },
+  {
+    "revision": "9d837414d3358bee39f879852ff25546",
+    "url": "/training_project_for__html_css/static/media/Layer2.9d837414.png"
+  },
+  {
+    "revision": "bdaf90625e2f010393fe2dda284d12d5",
+    "url": "/training_project_for__html_css/static/media/Layer2@2x.bdaf9062.png"
+  },
+  {
+    "revision": "000ae8452b4a228b9aaf0e168ad333a0",
+    "url": "/training_project_for__html_css/static/media/Layer3.000ae845.png"
+  },
+  {
+    "revision": "10e6dfcfbe65dc604eb59b640b454576",
+    "url": "/training_project_for__html_css/static/media/Layer3@2x.10e6dfcf.png"
+  },
+  {
+    "revision": "4dfc672d46d97033b95d1a650e3c2d51",
+    "url": "/training_project_for__html_css/static/media/cart.4dfc672d.svg"
+  },
+  {
+    "revision": "4499b17eed82687de41f6071a7157549",
+    "url": "/training_project_for__html_css/static/media/eagle@1X.4499b17e.png"
+  },
+  {
+    "revision": "8c703ccdd752648c2899eb5d1aac01fd",
+    "url": "/training_project_for__html_css/static/media/eagle@2X.8c703ccd.png"
+  },
+  {
+    "revision": "da26bb180fff9b78fcb061ed75551359",
+    "url": "/training_project_for__html_css/static/media/facebook.da26bb18.svg"
+  },
+  {
+    "revision": "af0c30900809ca8fa76aa49ce3da1461",
+    "url": "/training_project_for__html_css/static/media/glass.af0c3090.svg"
+  },
+  {
+    "revision": "add38239281d6870bdfa67043cc8a82f",
+    "url": "/training_project_for__html_css/static/media/instagram.add38239.svg"
+  },
+  {
+    "revision": "4d63a840d47acaa49339b2d6b92f5c40",
+    "url": "/training_project_for__html_css/static/media/kaushan-script-v6-latin-regular.4d63a840.woff"
+  },
+  {
+    "revision": "4e8aedcdb1b98ea690596ab48d95f49f",
+    "url": "/training_project_for__html_css/static/media/kaushan-script-v6-latin-regular.4e8aedcd.woff2"
+  },
+  {
+    "revision": "79982cd1f74c6fa7451bf9b37ead09ff",
+    "url": "/training_project_for__html_css/static/media/montserrat-v12-latin-700.79982cd1.woff2"
+  },
+  {
+    "revision": "957e93fbbe131a59791cd820d98b7109",
+    "url": "/training_project_for__html_css/static/media/montserrat-v12-latin-700.957e93fb.woff"
+  },
+  {
+    "revision": "501ce09c42716a2f6e1503a25eb174c9",
+    "url": "/training_project_for__html_css/static/media/montserrat-v12-latin-regular.501ce09c.woff2"
+  },
+  {
+    "revision": "f29d2b8559699b6beb5b29b25b8bc572",
+    "url": "/training_project_for__html_css/static/media/montserrat-v12-latin-regular.f29d2b85.woff"
+  },
+  {
+    "revision": "631824981751df72c441963a9638175e",
+    "url": "/training_project_for__html_css/static/media/pinterest.63182498.svg"
+  },
+  {
+    "revision": "210a7c781f5a354a0e4985656ab456d9",
+    "url": "/training_project_for__html_css/static/media/roboto-v18-latin-300italic.210a7c78.woff"
+  },
+  {
+    "revision": "d69924b98acd849cdeba9fbff3f88ea6",
+    "url": "/training_project_for__html_css/static/media/roboto-v18-latin-300italic.d69924b9.woff2"
+  },
+  {
+    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
+    "url": "/training_project_for__html_css/static/media/roboto-v18-latin-regular.5d4aeb4e.woff2"
+  },
+  {
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "/training_project_for__html_css/static/media/roboto-v18-latin-regular.bafb105b.woff"
+  },
+  {
+    "revision": "c11940304ad92e7a7a338324a352fab2",
+    "url": "/training_project_for__html_css/static/media/twitter.c1194030.svg"
+  }
+]);
